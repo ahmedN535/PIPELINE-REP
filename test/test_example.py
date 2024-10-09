4ac9d9d3ac677c73ab3bc4779c27b0e98ad5f5f0
Ahmed <go36rev@mytum.de>
2024-10-09 10:56:34 +0200

    initial

--- test/test_example.py
+++ test/test_example.py
@@ -1,1 +1,12 @@
-# Your testing code
+# Your testing code 
+from src.my_math import *
+
+def test_add():
+    result = add_numbers(1,2)
+    assert result == 3
+def test_sub():
+    result2 = subtract_numbers(1,2)
+    assert result2 == 1
+def test_mult():
+    result3 = multiply_numbers(2,2)
+    assert result3 == 4
